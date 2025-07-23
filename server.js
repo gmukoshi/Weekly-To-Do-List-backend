2cb161e59f0bbc8a24ab2377d24dd82347946f9d
George <mukoshig@gmail.com>
2025-07-24 01:03:53 +0300

    modifiedserver.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const jsonServer = require('json-server');
-const path = require('path');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ static: './' }); // Serve index.html
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use('/tasks', router); // Your API endpoint
+server.use('/tasks', router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
